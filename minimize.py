be200b313f3cff42cab5a2668dd5a994fd9b8202
James R. Maddison <j.r.maddison@ed.ac.uk>
2025-03-28 16:37:13 +0000

    Week 11 lecture

---
+++ minimize.py
@@ -0,0 +1,29 @@
+from scipy.optimize import minimize as sp_minimize
+
+import jax
+
+
+def minimize(fun, *args, **kwargs):
+    """Wrapper for :func:`scipy.optimize.minimize` using JAX for
+    gradient-based minimization. Automatically constructs `jac` from `fun`
+    using :func:`jax.jvp`.
+    """
+
+    J_vjp_cache = None
+
+    def J_vjp(x):
+        nonlocal J_vjp_cache
+        if J_vjp_cache is None or not (J_vjp_cache[0] == x).all():
+            J_vjp_cache = x.copy(), jax.vjp(fun, x)
+        return J_vjp_cache[1]
+
+    def J(x):
+        J_val, _ = J_vjp(x)
+        return J_val
+
+    def dJ(x):
+        _, vjp = J_vjp(x)
+        dJ_val, = vjp(1.0)
+        return dJ_val
+
+    return sp_minimize(J, *args, jac=dJ, **kwargs)
